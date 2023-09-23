adc08d88400ff44fb310eb5cf8538aac6eadd33b
Hitesh Meta <meta.hitesh85@gmail.com>
2023-09-24 01:01:54 +0530

    Shadcn Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Lora } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Layout from "@/pages/_layout";
+
+const lora = Lora({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={cn(lora.className)}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
---
+++ src/pages/_layout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import React from "react";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const meta = {
+    title: "SIH 2023 Web",
+    description: "SIH 2023 Web",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta content={meta.description} name="description" />
+      </Head>
+      <main>{children}</main>
+      <TailwindIndicator />
+    </>
+  );
+}
