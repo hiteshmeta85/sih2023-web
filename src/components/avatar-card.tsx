1e1abdbf672ba08938ed7da1321866b9cfa61742
Hitesh Meta <meta.hitesh85@gmail.com>
2023-09-25 17:09:57 +0530

    Add Dialog in Avatar Card

--- src/components/avatar-card.tsx
+++ src/components/avatar-card.tsx
@@ -1,8 +1,17 @@
 import Image from "next/legacy/image";
-import { Button } from "@/components/ui/button";
 import { AvatarIP } from "@/types";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
-export default function AvatarCard({ nickname, description, image }: AvatarIP) {
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
+export default function AvatarCard({
+  nickname,
+  description,
+  image,
+  intro_video,
+}: AvatarIP) {
   return (
     <div className="flex flex-col items-start space-y-4 rounded-2xl border p-4">
       <Image
@@ -17,9 +26,21 @@ export default function AvatarCard({ nickname, description, image }: AvatarIP) {
           <p className="text-2xl font-bold">{nickname}</p>
           <p className="text-sm text-muted-foreground">{description}</p>
         </div>
-        <Button type="button" variant="outline">
-          <>View Intro Video</>
-        </Button>
+        <Dialog>
+          <DialogTrigger className="hover:text-accent-foregroun inline-flex h-9 items-center justify-center rounded-md border border-input bg-transparent px-4 py-2 text-sm font-medium shadow-sm hover:bg-accent">
+            View Intro Video
+          </DialogTrigger>
+          <DialogContent>
+            <div className="aspect-video">
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                url={intro_video}
+                controls
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
