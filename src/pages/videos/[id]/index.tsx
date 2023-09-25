5939f0592f675814427a74169ac76d990723aef0
Hitesh Meta <meta.hitesh85@gmail.com>
2023-09-25 12:04:26 +0530

    Add react-player

--- src/pages/videos/[id]/index.tsx
+++ src/pages/videos/[id]/index.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { VIDEO_DATA } from "@/constants";
 
+// React Player
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
 export default function IndividualVideo() {
   const [videoData, setVideoData] = useState(VIDEO_DATA);
 
@@ -13,8 +17,20 @@ export default function IndividualVideo() {
         <p className="text-sm tracking-wide text-muted-foreground">
           {videoData.description}
         </p>
+        <p className="inline-flex h-9 items-center justify-center rounded-md border border-input bg-transparent px-4 py-2 text-sm font-medium shadow-sm">
+          {videoData.language}
+        </p>
       </div>
-      <div className="mt-10 grid grid-cols-3"></div>
+      <div className="mt-10 flex gap-4">
+        <div className="col-span-2 h-[450px] w-[800px] border-2 border-primary">
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            url="https://youtu.be/Uzuk4vraXpA?feature=shared"
+            controls
+          />
+        </div>
+      </div>
     </div>
   );
 }
