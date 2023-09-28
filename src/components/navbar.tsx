0c75c812e53297e4da50fe30105c3ddb5495bbf9
Hitesh Meta <meta.hitesh85@gmail.com>
2023-09-29 00:12:56 +0530

    Add Logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Logo from "@/components/logo";
 import Link from "next/link";
+import Image from "next/legacy/image";
 
 const Navbar = ({ children }: { children?: React.ReactNode }) => {
   return (
     <>
-      <nav className="container mx-auto flex flex-wrap items-center justify-between gap-4 p-4">
-        <Link href={"/"}>
-          <Logo />
-        </Link>
+      <nav className="container mx-auto flex flex-wrap items-center justify-between gap-4 px-4 py-1">
+        <div className="flex items-center gap-4">
+          <Link href={"/"}>
+            <Logo />
+          </Link>
+          {/*logo*/}
+          <div className="px-2 pt-2 dark:bg-white">
+            <Image
+              src="/images/ministry_of_information_and_broadcasting_logo.png"
+              alt="logo"
+              width={100}
+              height={56}
+            />
+          </div>
+        </div>
         {children}
       </nav>
       <hr />
